66d46dfc9f19b54b69eaacc1017a79eef4b80ed9
Grant Harris <grant@eatart.org>
2016-03-23 16:38:58 -0700

    Call parent functions

--- src/L.Popup.Angular.js
+++ src/L.Popup.Angular.js
@@ -3,32 +3,7 @@
  L.Popup.Angular = L.Popup.extend({
     _initLayout: function () {
         var that = this;
-        var prefix = 'leaflet-popup',
-            containerClass = prefix + ' ' + this.options.className + ' leaflet-zoom-' +
-                    (this._animated ? 'animated' : 'hide'),
-            container = this._container = L.DomUtil.create('div', containerClass),
-            closeButton;
-
-        if (this.options.closeButton) {
-            closeButton = this._closeButton =
-                    L.DomUtil.create('a', prefix + '-close-button', container);
-            closeButton.href = '#close';
-            closeButton.innerHTML = '&#215;';
-            L.DomEvent.disableClickPropagation(closeButton);
-
-            L.DomEvent.on(closeButton, 'click', this._onCloseButtonClick, this);
-        }
-
-        var wrapper = this._wrapper =
-                L.DomUtil.create('div', prefix + '-content-wrapper', container);
-        L.DomEvent.disableClickPropagation(wrapper);
-
-        this._contentNode = L.DomUtil.create('div', prefix + '-content', wrapper);
-
-        L.DomEvent.disableScrollPropagation(this._contentNode);
-        L.DomEvent.on(wrapper, 'contextmenu', L.DomEvent.stopPropagation);
-
-        this._tipContainer = L.DomUtil.create('div', prefix + '-tip-container', container);
+        L.Popup.prototype._initLayout.call(this);
 
         angular.element(document).ready(function() {
              // Grab the injector for the current angular app
@@ -39,14 +14,14 @@
                  $controller = $injector.get('$controller');
 
              that._scope = $rootScope.$new(true);
-             this._element = angular.element(this._contentNode);
-             this._element .html(that.options.template);
+             that._element = angular.element(that._contentNode);
+             that._element.html(that.options.template);
 
              if (that.options.controller) {
                  var controller = $controller(that.options.controller, {
                      '$map': map,
                      '$scope': that._scope,
-                     '$element': this._element,
+                     '$element': that._element,
                      '$options': that.options
                  });
 
@@ -54,15 +29,13 @@
                      that._scope[that.options.controllerAs] = controller;
                  }
 
-                 this._element.data('$ngControllerController', controller);
-                 this._element.children().data('$ngControllerController', controller);
+                 that._element.data('$ngControllerController', controller);
+                 that._element.children().data('$ngControllerController', controller);
              }
 
-             $compile(this._element)(that._scope);
+             $compile(that._element)(that._scope);
              that._scope.$apply();
          });
-
-        this._tip = L.DomUtil.create('div', prefix + '-tip', this._tipContainer);
     },
     _updateContent: function () {
         if (!this._content) { return; }
@@ -77,10 +50,11 @@
         }
         this.fire('contentupdate');
     },
-     onRemove: function(){
+     onRemove: function(map){
         if(this._scope){
             this._scope.$destroy();
         }
+        L.Popup.prototype.onRemove.call(map);
      }
  });
 
